b7c738d56b6f49d45c9526a09325406d921c68e7
joyzhao <2770723534@qq.com>
2024-06-06 22:09:55 +0800

    chore: add utilities to the project 20%

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Button, Space } from "antd";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,18 +19,27 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {/*<button onClick={() => setCount((count) => count + 1)}>*/}
+        {/*  count is {count}*/}
+        {/*</button>*/}
+        <p>count is {count}</p>
+        <Space size={12}>
+          <Button
+            type={"primary"}
+            onClick={() => setCount((count) => count + 1)}
+          >
+            加一
+          </Button>
+          <Button
+            type={"primary"}
+            onClick={() => setCount((count) => count - 1)}
+          >
+            减一
+          </Button>
+        </Space>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [react()],
-})
+});
